a56cbdec6f76997876ebfcfb3785ab51c3f234c9
sunjid-git <sunjid.info@gmail.com>
2021-10-08 22:44:29 +0600

    Contact Part

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from "./components/HeaderFooter/Header";
+import Contact from "./components/Contact/Contact";
 function App() {
   return (
     <Router>
     <Header/>
       <Switch>
-        <Route exact to="/" component={Home} />
-        <Route exact to="/home" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route  path="/home" component={Home} />
+        <Route path="/contact" component={Contact} />
       </Switch>
     </Router>
   );
--- src/components/HeaderFooter/Header.js
+++ src/components/HeaderFooter/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div>
       {/* navbar starts */}
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
-            Navbar
+      <nav className="font-b navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="#">
+            <Link to="/">Sunjid</Link>
           </a>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarNavDropdown"
@@ -19,56 +20,30 @@ const Header = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon "></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+              
+                <Link to="/home" className="nav-link active" aria-current="page">
+                Home
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Features
-                </a>
+              <li className="nav-item">
+              <Link to="/about" className="nav-link" >
+                  About
+                </Link>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  Pricing
-                </a>
+              <li className="nav-item">
+              <Link to="/project" className="nav-link" >
+                  Project
+                </Link>
               </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown link
-                </a>
-                <ul
-                  class="dropdown-menu"
-                  aria-labelledby="navbarDropdownMenuLink"
-                >
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link" >
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
